55554b9f9361e13bd2437bbf65a3e3774791bf17
zhouxun <zhxun@czbapp.com>
2018-06-21 14:22:07 +0800

    【update】【配置文件】修改

--- src/main/java/com/czb/article/util/FastJsonUtils.java
+++ src/main/java/com/czb/article/util/FastJsonUtils.java
@@ -12,86 +12,87 @@ import java.util.Map;
 
 public class FastJsonUtils {
 
-    private static final SerializeConfig config;
+	private static final SerializeConfig config;
 
-    static {
-        config = new SerializeConfig();
-        config.put(java.util.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
-        config.put(java.sql.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
-    }
+	static {
+		config = new SerializeConfig();
+		config.put(java.util.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
+		config.put(java.sql.Date.class, new JSONLibDataFormatSerializer()); // 使用和json-lib兼容的日期输出格式
+	}
 
-    private static final SerializerFeature[] features = {SerializerFeature.WriteMapNullValue, // 输出空置字段
-            SerializerFeature.WriteNullListAsEmpty, // list字段如果为null，输出为[]，而不是null
-            SerializerFeature.WriteNullNumberAsZero, // 数值字段如果为null，输出为0，而不是null
-            SerializerFeature.WriteNullBooleanAsFalse, // Boolean字段如果为null，输出为false，而不是null
-            SerializerFeature.WriteNullStringAsEmpty // 字符类型字段如果为null，输出为""，而不是null
-    };
+	private static final SerializerFeature[] features = { SerializerFeature.WriteMapNullValue, // 输出空置字段
+			SerializerFeature.WriteNullListAsEmpty, // list字段如果为null，输出为[]，而不是null
+			SerializerFeature.WriteNullNumberAsZero, // 数值字段如果为null，输出为0，而不是null
+			SerializerFeature.WriteNullBooleanAsFalse, // Boolean字段如果为null，输出为false，而不是null
+			SerializerFeature.WriteNullStringAsEmpty // 字符类型字段如果为null，输出为""，而不是null
+	};
 
-    public static String toJSONString(Object object) {
-        return JSON.toJSONString(object, config, features);
-    }
+	public static String toJSONString(Object object) {
+		return JSON.toJSONString(object, config, features);
+	}
 
-    public static String toJSONNoFeatures(Object object) {
-        return JSON.toJSONString(object, config, SerializerFeature.DisableCircularReferenceDetect);
-    }
+	public static String toJSONNoFeatures(Object object) {
+		return JSON.toJSONString(object, config, SerializerFeature.DisableCircularReferenceDetect);
+	}
 
+	public static Object toBean(String text) {
+		return JSON.parse(text);
+	}
 
-    public static Object toBean(String text) {
-        return JSON.parse(text);
-    }
+	public static <T> T toBeanClass(String text, Class<T> clazz) {
+		return JSON.parseObject(text, clazz);
+	}
 
-    public static <T> T toBeanClass(String text, Class<T> clazz) {
-        return JSON.parseObject(text, clazz);
-    }
+	// 转换为数组
+	public static <T> Object[] toArray(String text) {
+		return toArrayClass(text, null);
+	}
 
-    // 转换为数组
-    public static <T> Object[] toArray(String text) {
-        return toArrayClass(text, null);
-    }
+	// 转换为数组
+	public static <T> Object[] toArrayClass(String text, Class<T> clazz) {
+		return JSON.parseArray(text, clazz).toArray();
+	}
 
-    // 转换为数组
-    public static <T> Object[] toArrayClass(String text, Class<T> clazz) {
-        return JSON.parseArray(text, clazz).toArray();
-    }
+	// 转换为List
+	public static <T> List<T> toList(String text, Class<T> clazz) {
+		return JSON.parseArray(text, clazz);
+	}
 
-    // 转换为List
-    public static <T> List<T> toList(String text, Class<T> clazz) {
-        return JSON.parseArray(text, clazz);
-    }
+	/**
+	 * 将javabean转化为序列化的json字符串
+	 */
+	public static Object beanToJson(KeyValue keyvalue) {
+		String textJson = JSON.toJSONString(keyvalue);
+		return JSON.parse(textJson);
+	}
 
-    /**
-     * 将javabean转化为序列化的json字符串
-     */
-    public static Object beanToJson(KeyValue keyvalue) {
-        String textJson = JSON.toJSONString(keyvalue);
-        return JSON.parse(textJson);
-    }
+	/**
+	 * 将string转化为序列化的json字符串
+	 */
+	public static Object textToJson(String text) {
+		return JSON.parse(text);
+	}
 
-    /**
-     * 将string转化为序列化的json字符串
-     */
-    public static Object textToJson(String text) {
-        return JSON.parse(text);
-    }
+	/**
+	 * json字符串转化为map
+	 */
+	@SuppressWarnings("rawtypes")
+	public static Map stringToMap(String s) {
+		return JSONObject.parseObject(s);
+	}
 
-    /**
-     * json字符串转化为map
-     */
-    public static Map stringToMap(String s) {
-        return JSONObject.parseObject(s);
-    }
+	/**
+	 * 将map转化为string
+	 */
+	@SuppressWarnings("rawtypes")
+	public static String mapToString(Map m) {
+		return JSONObject.toJSONString(m, config, features);
+	}
 
-    /**
-     * 将map转化为string
-     */
-    public static String mapToString(Map m) {
-        return JSONObject.toJSONString(m, config, features);
-    }
-
-    /**
-     * 把对象转换为指定对象
-     */
-    public static <T> T toObjectFromSource(Object source, Class<T> target) {
-        return toBeanClass(toJSONString(source), target);
-    }
+	/**
+	 * 把对象转换为指定对象
+	 */
+	public static <T> T toObjectFromSource(Object source, Class<T> target) {
+		return toBeanClass(toJSONString(source), target);
+	}
 }
